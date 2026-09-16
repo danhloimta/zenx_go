af8d791125e1fed10b4f9c67f17b0b42f6346561
Nguyen Danh Loi <danhloimta@gmail.com>
2026-09-16 18:07:15 +0700

    fix: make player support migration idempotent

--- apps/api/prisma/migrations/202609180001_game_player_support/migration.sql
+++ apps/api/prisma/migrations/202609180001_game_player_support/migration.sql
@@ -1,7 +1,8 @@
 BEGIN TRY
   BEGIN TRAN;
 
-  ALTER TABLE [dbo].[game_players] ADD [support_note] NVARCHAR(2000) NULL;
+  IF COL_LENGTH('dbo.game_players', 'support_note') IS NULL
+    ALTER TABLE [dbo].[game_players] ADD [support_note] NVARCHAR(2000) NULL;
 
   COMMIT TRAN;
 END TRY
